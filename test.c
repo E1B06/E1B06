13f773419718bd40ffeb62b42d8df88a2b87cdc0
E1B06 <u11202206@o365.ttu.edu.tw>
2024-05-14 23:05:09 +0800

    test.c

--- test.c
+++ test.c
@@ -3,7 +3,15 @@
 
 int main(void) 
 {
+	int A[][2]={{1,3},{5,7},{2,4},{6,8}};
+	int b[10];
+	int *pi,**pp;
+	int (*p)[2];
+	p=a;
 	
-	printf("Hello World");
+	printf("%p\n",A);
+	//printf("%p\n",A);
 	
+	
+	return 0;
 }
